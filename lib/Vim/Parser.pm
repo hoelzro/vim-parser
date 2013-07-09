5ff88ce4f293a3ab666b209dc21e9596c9f450d1
Rob Hoelz <rob@hoelz.ro>
2013-07-09 19:21:18 +0200

    initial commit

---
+++ lib/Vim/Parser.pm
@@ -0,0 +1,22 @@
+## no critic (RequireUseStrict)
+package Vim::Parser;
+
+## use critic (RequireUseStrict)
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT: A short description of Vim::Parser
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head1 SEE ALSO
+
+=cut
